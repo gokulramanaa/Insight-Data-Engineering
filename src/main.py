bfb4dbf0401d7f10bb3d8b5b2f7c7b60be332b71
gokulramanaa <gokul_ramanan@hotmail.com>
2018-10-28 10:59:20 -0400

    WIP

--- src/main.py
+++ src/main.py
@@ -1,17 +1,53 @@
 
-
-file = open('../input/H1B_FY_2016.csv')
-
-print(type(file))
-with open('../input/test.csv') as f: s = f.read()
+def parseInputFile():
+    global certified_count
+    occupations, states = dict(), dict()
+    with open('../input/test.csv') as f: s = f.read()
+    content = s.split("\n")
     
-    
-print(type(s))
-print(s)
+    for i,j in enumerate(content[0].split(';')):
+        if j == "CASE_STATUS":
+            status_ind = i
+        if j == "SOC_CODE":
+            socode_ind = i
+        if j == "SOC_NAME":
+            socname_ind = i
+        if j == "EMPLOYER_STATE":
+            state_ind = i
+            
+    def parseRow():
+        fields = line.split(sep)
+        status = fields[status_ind].strip()
+        status = status.upper()
+        occupation = fields[socode_ind].strip()
+        occupation = occupation.upper()
+        state = fields[state_ind].strip()
+        state = state.upper()
 
-#k = 1
-#for i in file:
-#    print(i)
-#    if k ==3:
-#        break
-#    k+=1
+        if status == 'CERTIFIED':
+            if occupations.has_key(occupation):
+                tmpsoc = occupations[occupation]
+                tmpsoc +=1
+                occupations[occupation] = tmpsoc
+            else:
+                occupations[occupation] = 1 
+            if states.has_key(state):
+                tmpsoc = states[state]
+                tmpsoc +=1
+                states[state] = tmpsoc
+            else:
+                states[state] = 1 
+            certified_count +=1
+        
+    list(map(opr,l[:5]))
+
+    return  certified_count, occupation_dict, states_dict
+    
+def main():
+    global certified_count
+    certified_count = 0
+    certified_count,occupation_dict, states_dict = parseInputFile()
+    print(certified_count,occupation_dict, states_dict)
+
+if __name__ == "__main__":
+    main()
