d4bba0e1974b7dc2e0e8b169a895fd7672a2bef6
gokulramanaa <gokul_ramanan@hotmail.com>
2018-10-28 09:37:36 -0400

    update

---
+++ src/main.py
@@ -0,0 +1,17 @@
+
+
+file = open('../input/H1B_FY_2016.csv')
+
+print(type(file))
+with open('../input/test.csv') as f: s = f.read()
+    
+    
+print(type(s))
+print(s)
+
+#k = 1
+#for i in file:
+#    print(i)
+#    if k ==3:
+#        break
+#    k+=1
