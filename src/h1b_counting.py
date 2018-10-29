5d2956c630bacc240150886060bf5ec2f1206a80
Gokulramanan Soundararajan <31602253+gokulramanaa@users.noreply.github.com>
2018-10-29 11:49:05 -0400

    Update h1b_counting.py

--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -3,20 +3,13 @@ from Top10list import Top10list
 import sys
     
 def main():
-    #print(sys.argv)
-    #try:
-    #    filename, separator = sys.argv[1],';'
-    #except:
-    #    print("Please give the valid input file name from input folder")
-    #    return 
-    separator = ';' 
-    inputfile, outputoccupation, outputstate = sys.argv[1],sys.argv[2],sys.argv[3]
+    separator = ';' #global separator declaration 
+    inputfile, outputoccupation, outputstate = sys.argv[1],sys.argv[2],sys.argv[3] #input, output file paths
     InputHolder = ParseInput(inputfile, separator)
-    certified_count,occupation_dict, states_dict = InputHolder.getData()
-    #print(certified_count)
+    certified_count,occupation_dict, states_dict = InputHolder.getData() #get distribution of occupation and states
     Toppers = Top10list(certified_count,occupation_dict, states_dict,separator, outputstate,outputoccupation)
-    Toppers.top10Occupation()
-    Toppers.top10State()
+    Toppers.top10Occupation() #writes top 10 occupation to a file
+    Toppers.top10State() #writes top 10 states to a file
 
 if __name__ == "__main__":
     main()
