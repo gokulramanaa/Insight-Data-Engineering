d081faf6a4231183977ce94969e00df4fc5e0fd5
Gokulramanan Soundararajan <31602253+gokulramanaa@users.noreply.github.com>
2018-10-29 11:54:59 -0400

    Update Top10list.py

--- src/Top10list.py
+++ src/Top10list.py
@@ -8,10 +8,12 @@ class Top10list():
         self.occupationoutput = occop
         
     def top10Occupation(self):
+        #sort the occupation dictionary, first by count and by alphabetical order 
         sorted_occ_tuple = sorted(self.occupation_dict.items(), key=lambda kv: (-kv[1],kv[0]))[:10]
         file = open(self.occupationoutput,'w')
         file.write('TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE' + '\n')
         for each in sorted_occ_tuple:
+            #calculate percentage of approved per each occupation
             percentage =  (float(each[1])/self.certified_count) * 100
             file.write(str(each[0]) + self.sep + str(each[1]) + self.sep + str(percentage) + "%" + '\n')
         file.close()
